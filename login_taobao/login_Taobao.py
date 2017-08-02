3a7c74f090b70105725e7046a42d72e0e1a1c6cf
Hopegoing <zlwork2014@gmail.com>
2017-08-02 21:51:10 +0800

    登录可能直接进入淘宝首页，所以添加一个try

--- login_taobao/login_Taobao.py
+++ login_taobao/login_Taobao.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import time
 
 class loginTB(object):
     def __init__(self):
@@ -42,40 +43,46 @@ class loginTB(object):
                 user_info = self.wait.until(
                     EC.presence_of_element_located((By.XPATH,"//div[@class='m-userinfo']"))
                 )
-                print('不需要手机验证，已经登陆成功！！！')
+                print('已经登陆成功，进入了个人中心')
             except TimeoutException:
                 try:
-                    # 尝试找手机验证框，如果能找到说明要手机验证
-                    frame = self.wait.until(
-                        EC.presence_of_element_located((By.XPATH,'//div[@class="login-check-left"]/iframe'))
-                    )
-                    print('本次登录需要进行手机验证...')
-                except TimeoutException:
-                    # 找不到手机验证说明密码账号输入错误，要重新输入
-                    print('登录失败，目测是账号或密码有误，请检查后重新登录...')
-                    key = input('请重新输入账号：').strip()
-                    pw = input('请重新输入密码：').strip()
-                    self.login(key,pw)
-                else:
-                    self.driver.switch_to.frame(frame)
-                    phone_num = self.wait.until(
-                        EC.presence_of_element_located((By.XPATH,'//button[@id="J_GetCode"]'))
-                    )
-                    phone_num.click()
-                    phone_key = input('请输入手机验证码：').strip()
-                    key_send = self.wait.until(
-                        EC.presence_of_element_located((By.XPATH,'//input[@id="J_Phone_Checkcode"]'))
-                    )
-                    key_send.send_keys(phone_key)
-                    go_button = self.wait.until(
-                        EC.presence_of_element_located((By.XPATH,'//input[@type="submit"]'))
-                    )
-                    go_button.click()
-                    user_info = self.wait.until(
-                        EC.presence_of_element_located((By.XPATH, "//div[@class='m-userinfo']"))
-                    )
-                    print('手机验证登陆成功！！！')
+                    self.driver.find_element_by_xpath("//div[@class='avatar-wrapper']")
+                    print('已经登录成功，进入了淘宝网首页')
+                except:
+                    try:
+                        # 尝试找手机验证框，如果能找到说明要手机验证
+                        frame = self.wait.until(
+                            EC.presence_of_element_located((By.XPATH,'//div[@class="login-check-left"]/iframe'))
+                        )
+                        print('本次登录需要进行手机验证...')
+                    except TimeoutException:
+                        # 找不到手机验证说明密码账号输入错误，要重新输入
+                        print('登录失败，目测是账号或密码有误，请检查后重新登录...')
+                        key = input('请重新输入账号：').strip()
+                        pw = input('请重新输入密码：').strip()
+                        self.login(key,pw)
+                    else:
+                        self.driver.switch_to.frame(frame)
+                        phone_num = self.wait.until(
+                            EC.presence_of_element_located((By.XPATH,'//button[@id="J_GetCode"]'))
+                        )
+                        phone_num.click()
+                        phone_key = input('请输入手机验证码：').strip()
+                        key_send = self.wait.until(
+                            EC.presence_of_element_located((By.XPATH,'//input[@id="J_Phone_Checkcode"]'))
+                        )
+                        key_send.send_keys(phone_key)
+                        go_button = self.wait.until(
+                            EC.presence_of_element_located((By.XPATH,'//input[@type="submit"]'))
+                        )
+                        go_button.click()
+                        user_info = self.wait.until(
+                            EC.presence_of_element_located((By.XPATH, "//div[@class='m-userinfo']"))
+                        )
+                        print('手机验证登陆成功！！！')
 
 if __name__ == '__main__':
+    t = time.time()
     l = loginTB()
     l.login('username','password')
+    print('登录完成，耗时{:.2f}秒'.format(float(time.time()-t)))
